539180df30ea29b4aee7ca67bed0200b1c61eccc
MHZhao23 <minghan.zhao.21@gmail.com>
2025-11-24 17:38:49 +0000

    Flip and translation; 48 sequences

--- paste.py
+++ paste.py
@@ -142,19 +142,75 @@ def collect_tool_paths(group_folders):
         mask_paths.extend(masks)
     return img_paths, mask_paths
 
+def augmentation(oct_image, tool_image, oct_mask, tool_mask, s='l'):
+
+    # ---------------- Augmentation ----------------
+    # Brightness
+    # brightness_oct_i = brightness_oct + round(np.random.random(), 2) * 0.1
+    # brightness_tool_i = brightness_tool + round(np.random.random(), 2) * 0.1
+    brightness_oct = round(np.random.uniform(0.5, 1.2), 2)
+    brightness_tool = round(np.random.uniform(0.5, 1.1), 2)
+    oct_image = ImageEnhance.Brightness(oct_image).enhance(brightness_oct)
+    tool_image = ImageEnhance.Brightness(tool_image).enhance(brightness_tool)
+
+    # Crop and resize tools
+    tool_mask_array = np.array(tool_mask)
+    mask = (tool_mask_array == 2) | (tool_mask_array == 3)
+    x1, x2 = np.where(mask != 0)[1].min().item(), np.where(mask != 0)[1].max().item()
+    crop_box = (x1, 0, x2 + 300, 295)
+    tool_image = tool_image.crop(crop_box)
+    tool_mask = tool_mask.crop(crop_box)
+    translation = np.random.randint(0, 50)
+    tool_image = ImageOps.expand(tool_image.resize((img_size, img_size), Image.NEAREST), border=(0, 0, translation, translation), fill=0)
+    tool_mask = ImageOps.expand(tool_mask.resize((img_size, img_size), Image.NEAREST), border=(0, 0, translation, translation), fill=0)
+
+    oct_image_array = np.array(oct_image.resize((img_size, img_size), Image.NEAREST))
+    oct_mask_array = np.array(oct_mask.resize((img_size, img_size), Image.NEAREST))
+    tool_image_array = np.array(tool_image.resize((img_size, img_size), Image.NEAREST))
+    tool_mask_array = np.array(tool_mask.resize((img_size, img_size), Image.NEAREST))
+
+    # remove small artifact
+    mask = (tool_mask_array == 3).astype(np.uint8)
+    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(mask, connectivity=8)
+    for lbl in range(1, num_labels):
+        if stats[lbl, cv2.CC_STAT_AREA] < 300:
+            tool_mask_array[labels == lbl] = 0
+
+    # translation
+    mask = (tool_mask_array == 2) | (tool_mask_array == 3)
+    x_min, x_max = np.where(mask != 0)[1].min().item(), np.where(mask != 0)[1].max().item()
+    translation = int(((img_size - (x_max - x_min)) / num_oct_frames) * i) + np.random.randint(-3, 3)
+    if s == 'l':
+        left_pad = max(translation, 0)
+        right_pad = max(img_size - (x_max - x_min) - left_pad, 0)
+        if (left_pad + right_pad + (x_max - x_min)) > img_size:
+            left_pad = img_size - right_pad - (x_max - x_min)
+    if s == 'r':
+        right_pad = max(translation, 0)
+        left_pad = max(img_size - (x_max - x_min) - right_pad, 0)
+        if (left_pad + right_pad + (x_max - x_min)) > img_size:
+            right_pad = img_size - left_pad - (x_max - x_min)
+    assert (left_pad + right_pad + (x_max - x_min)) == img_size
+    tool_image_array = tool_image_array[:, x_min:x_max]
+    tool_image_array = np.pad(tool_image_array, pad_width=((0, 0), (left_pad, right_pad)), mode='constant', constant_values=0)    # (top, bottom), (left, right)
+    
+    tool_mask_array = tool_mask_array[:, x_min:x_max]
+    tool_mask_array = np.pad(tool_mask_array, pad_width=((0, 0), (left_pad, right_pad)), mode='constant', constant_values=0)    # (top, bottom), (left, right)
+
+    return oct_image_array, oct_mask_array, tool_image_array, tool_mask_array
+
 # Paths
 oct_root = "./datasets/AROI - online/24 patient"
 tools_root = "./datasets/tools"
 output_img_root = "./datasets/Synthetic_iOCT/train/JPEGImages"
-output_all_img_root = "./datasets/Synthetic_iOCT/train/JPEGImagesAll"
 output_mask_root = "./datasets/Synthetic_iOCT/train/Annotations"
 
 # Sort paths
 tool_groups = {
-    "Forceps_23": ["Forceps_23"],
-    "Scissors_25": [f"Scissors_25_{ch}" for ch in "ab"],
     "Cutter_25_1": [f"Cutter_25_1{ch}" for ch in "ab"],
-    "Cutter_25_2": [f"Cutter_25_2{ch}" for ch in "abcdefghijk"],
+    "Cutter_25_2": [f"Cutter_25_2{ch}" for ch in "ab"],
+    "Forceps_23": ["Forceps_23"],
+    "Scissors_25": ["Scissors_25_a"],
 }
 tool_sequences = {group_name: collect_tool_paths(group_folders) for group_name, group_folders in tool_groups.items()}
     
@@ -172,23 +228,24 @@ start_idx = 0
 img_size = 512
 patients = sorted(os.listdir(oct_root))
 for patient in tqdm(patients):
-    all_oct_dir = os.path.join(oct_root, patient, "raw/ALL")
     labelled_oct_dir = os.path.join(oct_root, patient, "raw/labeled")
     oct_mask_dir = os.path.join(oct_root, patient, "mask/number")
 
-    p_i = patient[7:]
+    p_i = int(patient[7:])
     output_img_dir = os.path.join(output_img_root, f"seq_{p_i}")
     output_mask_dir = os.path.join(output_mask_root, f"seq_{p_i}")
-    output_all_img_dir = os.path.join(output_all_img_root, f"seq_{p_i}")
     os.makedirs(output_img_dir, exist_ok=True)
     os.makedirs(output_mask_dir, exist_ok=True)
-    os.makedirs(output_all_img_dir, exist_ok=True)
 
-    all_oct_images = sorted([os.path.join(all_oct_dir, f) for f in os.listdir(all_oct_dir) if f.endswith(".png")])
+    output_img_dir_F = os.path.join(output_img_root, f"seq_{p_i + len(patients)}")
+    output_mask_dir_F = os.path.join(output_mask_root, f"seq_{p_i + len(patients)}")
+    os.makedirs(output_img_dir_F, exist_ok=True)
+    os.makedirs(output_mask_dir_F, exist_ok=True)
+
     labelled_oct_images = sorted([os.path.join(labelled_oct_dir, f) for f in os.listdir(labelled_oct_dir) if f.endswith(".png")])
     oct_mask_names = sorted([f for f in os.listdir(oct_mask_dir) if f.endswith(".png")])
     assert len(labelled_oct_images) == len(oct_mask_names)
-    num_oct_frames = len(all_oct_images) 
+    num_oct_frames = len(labelled_oct_images) 
 
     end_idx = start_idx + num_oct_frames
     for j in range(len(split_idx)-1):
@@ -216,50 +273,18 @@ for patient in tqdm(patients):
     # Generate synthetic frames
     # brightness_oct = np.random.random_integers(4, 11) * 0.1
     # brightness_tool = np.random.random_integers(4, 10) * 0.1
-    np.random.seed(int(p_i))
+    np.random.seed(p_i)
     p_flip = np.random.random()
     for i in range(num_oct_frames):
-        oct_name = os.path.basename(all_oct_images[i])
-        oct_image = Image.open(all_oct_images[i]).convert("L")
-        oct_mask = Image.open(os.path.join(oct_mask_dir, oct_name)).convert("L") if oct_name in oct_mask_names else None
+        oct_name = os.path.basename(labelled_oct_images[i])
+        oct_image = Image.open(labelled_oct_images[i]).convert("L")
+        oct_mask = Image.open(os.path.join(oct_mask_dir, oct_name)).convert("L")
         tool_image = Image.open(tool_images[i]).convert("L")
         tool_mask = Image.open(tool_masks[i]).convert("P")
 
-        # ---------------- Augmentation ----------------
-        # Brightness
-        # brightness_oct_i = brightness_oct + round(np.random.random(), 2) * 0.1
-        # brightness_tool_i = brightness_tool + round(np.random.random(), 2) * 0.1
-        brightness_oct = round(np.random.uniform(0.5, 1.2), 2)
-        brightness_tool = round(np.random.uniform(0.5, 1.1), 2)
-        oct_image = ImageEnhance.Brightness(oct_image).enhance(brightness_oct)
-        tool_image = ImageEnhance.Brightness(tool_image).enhance(brightness_tool)
-
-        # Crop tools
-        tool_mask_array = np.array(tool_mask)
-        translation = int((250 / num_oct_frames) * i + 50) + np.random.randint(-3, 3) # np.random.randint(50, 150)
-        mask = (tool_mask_array == 2) | (tool_mask_array == 3)
-        x1, x2 = np.where(mask != 0)[1].min().item(), np.where(mask != 0)[1].max().item()
-        crop_box = (x1 - translation, 0, x2 + (300 - translation), 295)
-        tool_image = tool_image.crop(crop_box)
-        tool_mask = tool_mask.crop(crop_box)
-
-        if p_flip > 0.5:
-            tool_image = tool_image.transpose(Image.FLIP_LEFT_RIGHT)
-            tool_mask = tool_mask.transpose(Image.FLIP_LEFT_RIGHT)
-
-        oct_image_array = np.array(oct_image.resize((img_size, img_size), Image.NEAREST))
-        oct_mask_array = np.array(oct_mask.resize((img_size, img_size), Image.NEAREST)) if oct_mask else None
-        tool_image_array = np.array(tool_image.resize((img_size, img_size), Image.NEAREST))
-        tool_mask_array = np.array(tool_mask.resize((img_size, img_size), Image.NEAREST))
+        oct_image_array, oct_mask_array, tool_image_array, tool_mask_array = augmentation(oct_image, tool_image, oct_mask, tool_mask, s = 'l')
 
         # ---------------- image paste ----------------
-        # remove small connected components for class == 3
-        mask = (tool_mask_array == 3).astype(np.uint8)
-        num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(mask, connectivity=8)
-        for lbl in range(1, num_labels):
-            if stats[lbl, cv2.CC_STAT_AREA] < 50:   # small artifact
-                tool_mask_array[labels == lbl] = 0
-
         mask = (tool_mask_array == 2) | (tool_mask_array == 3)
         translation = 10 + np.random.randint(-3, 3)
         x1, x2 = np.where(mask != 0)[1].min().item() - translation, np.where(mask != 0)[1].max().item() + translation
@@ -269,16 +294,37 @@ for patient in tqdm(patients):
         oct_image_array[mask] = tool_image_array[mask]
 
         # ---------------- mask paste ----------------
-        if oct_mask:
-            oct_mask_array[oct_mask_array == 4] = 0
-            oct_mask_array[oct_mask_array != 0] = 1
-            oct_mask_array[:, x1:x2] = 0
-            oct_mask_array[mask] = tool_mask_array[mask]
+        oct_mask_array[oct_mask_array == 4] = 0
+        oct_mask_array[oct_mask_array != 0] = 1
+        oct_mask_array[:, x1:x2] = 0
+        oct_mask_array[mask] = tool_mask_array[mask]
 
         out_name = oct_name.split('_')[1][3:]
-        oct_image = Image.fromarray(oct_image_array)
-        oct_image.save(os.path.join(output_all_img_dir, out_name))
-        if oct_mask: 
-            oct_image.save(os.path.join(output_img_dir, out_name))
-            save_ann_png(os.path.join(output_mask_dir, out_name), oct_mask_array)
+        oct_image_new = Image.fromarray(oct_image_array)
+        oct_image_new.save(os.path.join(output_img_dir, out_name))
+        save_ann_png(os.path.join(output_mask_dir, out_name), oct_mask_array)
 
+
+        tool_image_F = tool_image.transpose(Image.FLIP_LEFT_RIGHT)
+        tool_mask_F = tool_mask.transpose(Image.FLIP_LEFT_RIGHT)
+        oct_image_array, oct_mask_array, tool_image_array, tool_mask_array = augmentation(oct_image, tool_image_F, oct_mask, tool_mask_F, s = 'r')
+
+        # ---------------- image paste ----------------
+        mask = (tool_mask_array == 2) | (tool_mask_array == 3)
+        translation = 10 + np.random.randint(-3, 3)
+        x1, x2 = np.where(mask != 0)[1].min().item() - translation, np.where(mask != 0)[1].max().item() + translation
+        x1 = 0 if x1 < 20 else x1
+        x2 = img_size if abs(img_size - x2) < 20 else x2
+        oct_image_array[:, x1:x2] = 0
+        oct_image_array[mask] = tool_image_array[mask]
+
+        # ---------------- mask paste ----------------
+        oct_mask_array[oct_mask_array == 4] = 0
+        oct_mask_array[oct_mask_array != 0] = 1
+        oct_mask_array[:, x1:x2] = 0
+        oct_mask_array[mask] = tool_mask_array[mask]
+
+        out_name = oct_name.split('_')[1][3:]
+        oct_image_new = Image.fromarray(oct_image_array)
+        oct_image_new.save(os.path.join(output_img_dir_F, out_name))
+        save_ann_png(os.path.join(output_mask_dir_F, out_name), oct_mask_array)
